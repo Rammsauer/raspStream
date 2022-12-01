c7e0a1b3d7272493781ddf805f5315023f93de21
l.rudolph <leonr502@gmail.com>
2022-12-01 01:04:26 +0100

    rasp

--- YoutubePlayer.py
+++ YoutubePlayer.py
@@ -1,30 +1,29 @@
 import time
 import random
-import os
 import webbrowser
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller as kController
+from pynput.mouse import Listener, Button, Controller as mController
 
 
 def openYoutube(element, i):
-    keyboard = Controller()
+    keyboard = kController()
+    mouse = mController()
 
     print(element.name)
     point = 0
 
     if element.type == 1:
         point = element.length - random.randrange(1, element.length)
-        str = f'{element.link.replace("watch?v=", "tv#/")}&t={point}s'
+        str = f'{element.link.replace("/wa", "/tv#/")}&t={point}s'
         webbrowser.open(str)
     else:
-        webbrowser.open(element.link.replace("watch?v=", "tv#/"))
+        webbrowser.open(element.link.replace("/wa", "/tv#/"))
 
-    #time.sleep(35)
+    time.sleep(10)
 
-    #keyboard.press(Key.space)
-    #keyboard.release(Key.space)
-
-    #keyboard.press('f')
-    #keyboard.release('f')
+    mouse.click(Button.left)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 
     if element.type == 1:
         if point < 1200:
@@ -33,12 +32,6 @@ def openYoutube(element, i):
             time.sleep(random.uniform(660, 1200))
     else:
         time.sleep(random.uniform(660, 1200))
-
-
-    keyboard.press(Key.esc)
-    keyboard.release(Key.esc)
-
-    time.sleep(random.uniform(1, 3))
 
     keyboard.press(Key.ctrl_l)
     keyboard.press('w')

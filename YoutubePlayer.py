e185f1df3ad1a3f6d962b7536d5d24d21c233b5d
l.rudolph <leonr502@gmail.com>
2022-12-15 01:32:40 +0100

    rasp

--- YoutubePlayer.py
+++ YoutubePlayer.py
@@ -12,21 +12,21 @@ def openYoutube(element):
 
     imageLogIn = None
     timeStamp = time.time()
-    point = (element.length - random.randrange(1, (element.length - 600))) if element.type == 1 else 1
-    sleep = random.uniform(60, (element.length - point)) if element.type == 1 and (element.length - point) < 1200 else random.uniform(660, 1200)
+    point = (element.length - random.randrange(int(element.length/2), element.length)) if element.type == 1 else 1
+    sleep = random.uniform(int(element.length*0.8), (element.length - point)) if element.type == 1 and (element.length - point) < 1200 else random.uniform(660, 1200)
 
     print(f'{element.link.replace("https://www.youtube.com/watch?v=", "")} | {int(sleep)}s | {element.name} ')
 
     if element.type == 1:
-        str = f'{element.link.replace("/wa", "/tv#/")}&t={point}s'
-        webbrowser.open(str)
-    else:
-        webbrowser.open(element.link.replace("/wa", "/tv#/"))
+        webbrowser.open(f'{element.link}&t={point}s')
 
+    else:
+        webbrowser.open(element.link)
+
+    '''
     while imageLogIn is None:
         try:
             location1 = pyautogui.locateCenterOnScreen('img1.png')
-            location2 = pyautogui.locateCenterOnScreen('img3.png')
             if time.time() - timeStamp > 90:
                 keyboard.press(Key.ctrl_l)
                 time.sleep(5)
@@ -36,17 +36,18 @@ def openYoutube(element):
                 keyboard.release(Key.ctrl_l)
 
                 return
-            if location1 or location2:
+            if location1:
                 imageLogIn = True
         except Exception as e:
             imageLogIn = None
-
-    time.sleep(5)
+    
+    time.sleep(2)
 
     mouse.click(Button.left)
 
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
+    '''
 
     time.sleep(sleep)
 
@@ -59,4 +60,3 @@ def openYoutube(element):
     keyboard.release('w')
     keyboard.release(Key.ctrl_l)
 
-    time.sleep(5)
